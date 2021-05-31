866568b62bc9d9db82df31ccee435984e0f3bc81
Gabriel Lucas <gls.facom@gmail.com>
2021-05-31 16:50:17 -0400

    Arrumando bug de sample no vectorgrid, adicionando interpolação e plot interativo

--- gabriel/finite_diferences.py
+++ gabriel/finite_diferences.py
@@ -12,9 +12,10 @@ class VectorGrid2:
 
         x_points = int(resolution[0]/float(grid_spacing[0]))
         y_points = int(resolution[1]/float(grid_spacing[1]))
-        x = np.linspace(0, resolution[0] - grid_spacing[0], x_points)
-        y = np.linspace(0, resolution[1] - grid_spacing[1], y_points)
+        x = np.linspace(0.5*grid_spacing[0], resolution[0] - 0.5 * grid_spacing[0], x_points)
+        y = np.linspace(0.5*grid_spacing[1], resolution[1] - 0.5 * grid_spacing[1], y_points)
         
+        # TODO melhorar o jeito que estão salvas as coords
         self.x = x
         self.y = y
         xx, yy = np.meshgrid(x,y)
@@ -25,8 +26,9 @@ class VectorGrid2:
             for i in range(y_points):
                 for j in range(x_points):
                     #iterate over columns which is equivalent to iterate over x axis
-                    self.data[i][j] = sample(x[j],y[i])
-                    self.u[i][j], self.v[i][j] = self.data[i][j]
+                    self.u[i][j] = sample(x[j]-0.5*grid_spacing[0],y[i])[0]
+                    self.v[i][j] = sample(x[j],y[i]-0.5*grid_spacing[1])[1]
+                    self.data[i][j] = self.u[i][j], self.v[i][j]
         elif data is not None:
             self.data = data.copy()
             m, n, _ = self.data.shape
@@ -60,36 +62,63 @@ class VectorGrid2:
             print("no border case")
             return
 
+        # para u
         dx, dy = self.grid_spacing
-        x0, y0 = 0.5*self.grid_spacing[0], 0.5*self.grid_spacing[1] #origin of grid
-        i = (x-x0)//dx
-        j = (y-y0)//dy
+        x0, y0 = 0, 0 #origin of grid
+        ui = int((x-x0)/dx)
+        uj = int((y-y0)/dy-0.5)
         
-        xi = i+0.5*self.grid_spacing[0]
-        yj = j+0.5*self.grid_spacing[1]
-        x1, y1, q11 = xi, yj, self[int(i),int(j)]
-        x2, _y1, q21 = xi+1, yj, self[int(i+1),int(j)]
-        _x1, y2, q12 = xi, yj+1, self[int(i),int(j+1)]
-        _x2, _y2, q22 = xi+1, yj+1, self[int(i+1),int(j+1)]
+        xi = self.x[ui] - 0.5*self.grid_spacing[0]
+        xj = self.y[uj]
+        x1, y1, q11 = xi, xj, self[(ui),(uj)][0]
+        x2, _y1, q21 = xi+self.grid_spacing[0], xj, self[(ui+1),(uj)][0]
+        _x1, y2, q12 = xi, xj+self.grid_spacing[1], self[(ui),(uj+1)][0]
+        _x2, _y2, q22 = xi+self.grid_spacing[0], xj+self.grid_spacing[1], self[(ui+1),(uj+1)][0]
 
         if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
             raise ValueError('points do not form a rectangle')
         if not x1 <= x <= x2 or not y1 <= y <= y2:
             raise ValueError('(x, y) not within the rectangle')
 
-        return (q11 * (x2 - x) * (y2 - y) +
+        u = (q11 * (x2 - x) * (y2 - y) +
+                q21 * (x - x1) * (y2 - y) +
+                q12 * (x2 - x) * (y - y1) +
+                q22 * (x - x1) * (y - y1)
+            ) / ((x2 - x1) * (y2 - y1) + 0.0)
+        
+        # para v
+        dx, dy = self.grid_spacing
+        x0, y0 = 0, 0 #origin of grid
+        vi = int((x-x0)/dx-0.5)
+        vj = int((y-y0)/dy)
+        
+        yi = self.x[vi] 
+        yj = self.y[vj] - 0.5*self.grid_spacing[1]
+        x1, y1, q11 = yi, yj, self[(vi),(vj)][1]
+        x2, _y1, q21 = yi+self.grid_spacing[0], yj, self[(vi+1),(vj)][1]
+        _x1, y2, q12 = yi, yj+self.grid_spacing[1], self[(vi),(vj+1)][1]
+        _x2, _y2, q22 = yi+self.grid_spacing[0], yj+self.grid_spacing[1], self[(vi+1),(vj+1)][1]
+
+        if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
+            raise ValueError('points do not form a rectangle')
+        if not x1 <= x <= x2 or not y1 <= y <= y2:
+            raise ValueError('(x, y) not within the rectangle')
+
+        v = (q11 * (x2 - x) * (y2 - y) +
                 q21 * (x - x1) * (y2 - y) +
                 q12 * (x2 - x) * (y - y1) +
                 q22 * (x - x1) * (y - y1)
             ) / ((x2 - x1) * (y2 - y1) + 0.0)
 
-    def plot(self, stream=False):
+        return [u,v]
+
+    def plot(self, ax, stream=False):
         if stream:
-            plt.streamplot(self.x,self.y,self.u,self.v)
+            ax.streamplot(self.x,self.y,self.u,self.v)
         else:
-            plt.quiver(self.x, self.y + 0.5 * self.grid_spacing[1], self.u, np.zeros_like(self.u))
-            plt.quiver(self.x + 0.5 * self.grid_spacing[0], self.y, np.zeros_like(self.v), self.v)
-        plt.grid(True)
+            ax.quiver(self.x - 0.5*self.grid_spacing[0], self.y, self.u, np.zeros_like(self.u))
+            ax.quiver(self.x, self.y- 0.5*self.grid_spacing[0], np.zeros_like(self.v), self.v)
+        ax.grid(True)
         
     def divergent(self):
         m, n, _ = self.data.shape
@@ -209,6 +238,9 @@ class ScalarGrid2:
                 q22 * (x - x1) * (y - y1)
             ) / ((x2 - x1) * (y2 - y1) + 0.0)
 
+    def plot(self, ax):
+        ax.pcolormesh(self.x, self.y, self.data, shading='auto')
+
     def gradient(self):
         m, n = self.data.shape
 
@@ -266,10 +298,21 @@ def f1(x, y):
     # return np.sin(x) * np.sin(y)
 
 def f2(x, y):
-    # return np.asarray([1,1])
+    return np.asarray([1,1])
     # return np.asarray([x,y])
-    return np.asarray([np.sin(x),np.sin(y)])
+    # return np.asarray([np.sin(x),np.sin(y)])
     # return np.asarray([-y, x])
+
+def rmse(result, ref):
+    '''Compare two floats
+    output:
+        -rse: root mean squared error of a against b
+    '''
+    if type(ref) == np.ndarray:
+        size = 2
+    else:
+        size = 1 
+    return np.sqrt(np.sum(np.square(result - ref))/size)
 
 def get_coords_from_figure(grid):
     '''interative plot to calculate interpolation on clicked coord
@@ -278,12 +321,23 @@ def get_coords_from_figure(grid):
     def onclick(event):
         nonlocal ev
         ev = event
-        print(ev.xdata+ev.ydata, "   ", grid.interpolate(ev.xdata,ev.ydata))
+        if type(grid) == VectorGrid2:
+            u, v = grid.interpolate(ev.xdata,ev.ydata)
+            ref = f2(ev.xdata,ev.ydata)
+            ax.quiver(ev.xdata,ev.ydata, u, v)
+            print("RMSE: ", rmse([u,v],ref))
+            print(ref, "   ", [u,v])
+        elif type(grid) == ScalarGrid2:
+            res = grid.interpolate(ev.xdata,ev.ydata)
+            ref = f1(ev.xdata,ev.ydata)
+            ax.scatter(ev.xdata,ev.ydata)
+            ax.annotate("%.3f"%res, (ev.xdata,ev.ydata))
+            print("RMSE: ", rmse(ref, res))
+            print(ref, "   ", res)
 
+    plt.ion()
     fig, ax = plt.subplots()
-    # ax.title("sin(x)*sin(y)")
-    ax.pcolormesh(grid.x, grid.y, grid.data, shading='auto')
-    # ax.colorbar()
+    grid.plot(ax)
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
     plt.show(block=True)
@@ -313,6 +367,7 @@ def main():
     plt.show()
     
     # vec_grid = VectorGrid2(grids_res, grids_spc, f2)
+    # get_coords_from_figure(vec_grid)
     # div = vec_grid.divergent()
     # lapl = vec_grid.laplacian()
     # plt.title("Vector field divergent laplacian")
